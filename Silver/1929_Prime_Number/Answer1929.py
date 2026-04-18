8ff09daba5a63eaabd4c29208ea4ac3bf5ffdb49
sinseiki <eekdland@gmail.com>
2026-04-18 19:09:46 +0900

    Remove some files

--- Silver/1929_Prime_Number/Answer1929.py
+++
@@ -1,9 +0,0 @@
-m, n = map(int, input().split(' '))
-a = [1] * n
-i = 2
-while i <= n/i:
-  if a[i - 1] == 1:
-    for j in range(i, int(n/i) + 1): a[i * j - 1] = 0
-  i += 1
-for i in range(([1, m - 1][m > 1]), n):
-  if a[i] > 0: print(i + 1)
